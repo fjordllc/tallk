c05ff958fae61c726cd955c5f0ce06074f3841a7
KishiKyosuke <carp011808pg@icloud.com>
2020-12-07 13:53:19 +0900

    `admin/rooms_controller.rb`にshowアクションと質問箱管理画面の認証メソッドを追加
    - 入力されたパスワードが間違っている場合、質問箱のトップ画面にリダイレクトされます

--- app/controllers/admin/rooms_controller.rb
+++ app/controllers/admin/rooms_controller.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 class Admin::RoomsController < AdminController
+  skip_before_action :basic_auth, only: :show
+  before_action :set_room, only: :show
+  before_action :question_box_auth, only: :show
+
   def index
     @rooms = Room.page(params[:page])
+  end
+
+  def show
+    @topics = @room.topics.page(params[:page])
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def question_box_auth
+    redirect_to room_path(@room), notice: '正しいパスワードを入力してください' unless @room.authenticate(params[:entered_password])
   end
 end
