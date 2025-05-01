34fed7c3e3a1827ca35c7cb7f88753166a928927
komagata <komagata@gmail.com>
2025-05-01 09:01:11 +0900

    Fix deployment issues: update paths and entrypoint

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails
-rm -f /app/tmp/pids/server.pid
+rm -f /rails/tmp/pids/server.pid
 
 # If running the rails server, wait for the database to be ready
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
